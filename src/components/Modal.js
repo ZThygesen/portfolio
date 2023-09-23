fc90d92cd2fbcd538c92423414d47d361cabad6b
ZThygesen <zsthygesen@gmail.com>
2023-09-22 23:08:23 -0500

    add starry background

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,9 @@
 import { Modal as ModalContainer } from '../styles/Modal';
 
 export default function Modal({isOpen, closeModal, image}) {
+    // disable background scrolling when modal is open
+    document.querySelector('body').style.overflow = isOpen ? 'hidden' : 'auto';
+
     return (
         <ModalContainer style={{ display: isOpen ? 'flex' : 'none' }} onClick={closeModal}>
             <button className="material-icons close-modal" onClick={closeModal}>close</button>
